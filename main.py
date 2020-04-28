1c2aa387e85aa734052646fc93a7d377498ef4b7
Narek Baghdasaryan <narek2011n@gmail.com>
2020-04-28 23:39:15 +0400

    Removed some lines

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 print("Hello, World!")
 
 print("Test message")
-=======
+
 for i in range(10):
   print("Hello, World!")
 
 for i in "GitHub":
   print(i, end=" ")
 print()
->>>>>>> e3ccecba7b69c414487077f3ddde3d74cd0262f8
