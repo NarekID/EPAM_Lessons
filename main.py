77fcaa38b157f425ce3e43d00414a617c1cad6d1
Narek Baghdasaryan <narek2011n@gmail.com>
2020-04-30 13:52:01 +0400

    conflict solving lesson - feature branch

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ b = 15
 c = 100
 print(f'{a} + {b} + {c} = {a + b + c}')
 
-print('Non fast-forward merge test')
+print('Some text for conflig solving lesson')
+print('new content in feature branch')
