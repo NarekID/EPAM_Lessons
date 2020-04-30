4e80eaf4ad735bb2b96b80a660eed4ef6c0461b1
Narek Baghdasaryan <narek2011n@gmail.com>
2020-04-30 14:12:47 +0400

    add changes in master branch

--- main.py
+++ main.py
@@ -9,10 +9,4 @@ b = 15
 c = 100
 print(f'{a} + {b} + {c} = {a + b + c}')
 
-# <<<<<<< HEAD
-print('Text for conflict solving lesson')
-print('Changes from master branch')
-# =======
-print('Some text for conflig solving lesson')
-print('new content in feature branch')
-# >>>>>>> feature
+print('Our changes')
