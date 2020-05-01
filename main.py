c63ad57b8f5ed3dff10b4f5e72023b0de014bfc8
Narek Baghdasaryan <narek2011n@gmail.com>
2020-05-01 14:13:28 +0400

    some critical bugs fixed

--- main.py
+++ main.py
@@ -14,3 +14,7 @@ print(f'{a} / {b} / {c} = {a / b / c}')
 print(f'{a} = {bin(a)}')
 print(f'{b} = {bin(b)}')
 print(f'{c} = {bin(c)}')
+
+print(f'Message for pull request lesson')
+print(f'Some content and bugs fix here')
+
